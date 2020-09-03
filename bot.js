a90cef00ffe7e21e5846a550c487356a6c3a5185
Haxmatic <70683048+Haxmatic@users.noreply.github.com>
2020-09-03 01:36:22 -0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./auth.json');
+const config = require('./package.json');
+
+client.on('ready', () => {
+    console.log('Ready to spy on ' + client.users.size + ' users, in ' + client.channels.size + ' channels of ' + client.guilds.size + ' servers as ' + client.user.tag + '.');
+});
+
+client.on('message', (msg) => {
+    if (msg.author.username === "Recruit"){
+      let applicantName = "";
+      msg.embeds.forEach((embed) => {
+          applicantName = embed.fields[0].value;
+          makeChannel(msg, embed.fields[0],embed.fields[0].value);
+      });
+    //msg.reply("Application thread created for " + applicantName);
+    }
+});
+
+client.on('warn', (warn) => console.warn(warn));
+client.on('error', (error) => console.error(error));
+
+async function makeChannel(message, embed, name) {
+
+  let channelName = (name.split("#")[0] === "") ? "new-app" : name.split("#")[0];
+  let category = message.guild.channels.find(c => c.name == "Recruitment" && c.type == "category");
+
+  let channel = await message.guild.createChannel(channelName, { type: 'text' })
+  .then(async m => {
+    await m.setParent(category.id);
+    await m.send(embed);
+    await m.lockPermissions();
+    await console.log })
+  .catch(console.error);
+}
+
+client.login(auth.token);
