272f52a441a7aca0e5c8927f5b6edb32808ae159
Haxmatic <70683048+Haxmatic@users.noreply.github.com>
2024-04-09 13:50:07 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,3 @@
-const express = require('express')
-const app = express();
-const port = 3000
-
-app.get('/', (req, res) => res.send('Bot is now running!!'))
-
-app.listen(port, () =>
-  console.log(`Your app is listening a http://localhost:${port}`)
-);
 const mySecret = process.env['TOKEN'];
 const Discord = require('discord.js');
 const client = new Discord.Client();
