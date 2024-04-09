7e1aaf6ada91bcb51e8976c476f78e4cd740d1d8
Haxmatic <70683048+Haxmatic@users.noreply.github.com>
2024-04-09 13:46:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,31 @@
+const express = require('express')
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => res.send('Bot is now running!!'))
+
+app.listen(port, () =>
+  console.log(`Your app is listening a http://localhost:${port}`)
+);
+const mySecret = process.env['TOKEN'];
 const Discord = require('discord.js');
 const client = new Discord.Client();
 //const auth = require('./auth.json');
-const config = require('./package.json');
+//const config = require('./package.json');
 
 client.on('ready', () => {
-    console.log('Ready to spy on ' + client.users.size + ' users, in ' + client.channels.size + ' channels of ' + client.guilds.size + ' servers as ' + client.user.tag + '.');
+  console.log('Ready to spy on ' + client.users.size + ' users, in ' + client.channels.size + ' channels of ' + client.guilds.size + ' servers as ' + client.user.tag + '.');
 });
 
 client.on('message', (msg) => {
-    if (msg.author.username === "Recruit"){
-      let applicantName = "";
-      msg.embeds.forEach((embed) => {
-          applicantName = embed.fields[0].value;
-          makeChannel(msg, embed.fields[0],embed.fields[0].value);
-      });
+  if (msg.author.username === "Recruit") {
+    let applicantName = "";
+    msg.embeds.forEach((embed) => {
+      applicantName = embed.fields[0].value;
+      makeChannel(msg, embed.fields[0], embed.fields[0].value);
+    });
     //msg.reply("Application thread created for " + applicantName);
-    }
+  }
 });
 
 client.on('warn', (warn) => console.warn(warn));
@@ -24,15 +34,18 @@ client.on('error', (error) => console.error(error));
 async function makeChannel(message, embed, name) {
 
   let channelName = (name.split("#")[0] === "") ? "new-app" : name.split("#")[0];
-  let category = message.guild.channels.find(c => c.name == "Recruitment" && c.type == "category");
+  //let category = message.guild.channels.find(c => c.name == "Recruitment" && c.type == "category");
+  let category = message.guild.channels.find(c => c.name === "Recruitment");
+
 
   let channel = await message.guild.createChannel(channelName, { type: 'text' })
-  .then(async m => {
-    await m.setParent(category.id);
-    await m.send(embed);
-    await m.lockPermissions();
-    await console.log })
-  .catch(console.error);
+    .then(async m => {
+      await m.setParent(category.id);
+      await m.lockPermissions();
+      m.send(embed);
+      console.log
+    })
+    .catch(console.error);
 }
 
-client.login(process.env.BOT_TOKEN);
+client.login(mySecret);
